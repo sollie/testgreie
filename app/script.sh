aaf1f8316d09972cc2e76661859ca6addd98f6cf
Pål Sollie <pal.sollie@telia.no>
2022-07-01 12:35:13 +0200

    .

--- app/script.sh
+++ app/script.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-RESULT=`dig +short +time=3 +tries=5 nordicom.no`
+RESULT=`dig +short +time=2 +tries=2 nordicom.no`
 
 date
 if [ "${RESULT}" == ";; connection timed out; no servers could be reached" ]; then
